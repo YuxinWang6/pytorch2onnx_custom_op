1188184e6568d41d3def261630e5f1373b315de7
yuxin <yuxin.wang@enflame-tech.com>
2021-08-17 14:49:10 +0800

    try2

--- run_custom_op_onnxruntime_try.py
+++ run_custom_op_onnxruntime_try.py
@@ -8,20 +8,14 @@ from onnxruntime_extensions import (
     onnx_op, PyCustomOpDef,
     get_library_path as _get_library_path)
 
+
 @onnx_op(op_type="try_ncrelu_forward")
 def neg_vec(x):
     return -x
 
-# shared_library = '.pytorch_custom_op/try_ncrelu_forward.cpython-37m-x86_64-linux-gnu.so'
-shared_library = './pytorch_custom_op/build/lib.linux-x86_64-3.7/try_ncrelu_forward.cpython-37m-x86_64-linux-gnu.so'
-if not os.path.exists(shared_library):
-    raise FileNotFoundError("Unable to find '{0}'".format(shared_library))
-
-
 so1 = rt.SessionOptions()
-so1.register_custom_ops_library(shared_library)
-# so1.register_custom_ops_library(_get_library_path())
+# so1.register_custom_ops_library(shared_library)
+so1.register_custom_ops_library(_get_library_path())
 
 x_data = np.random.rand(3, 2, 1, 8).astype(np.float32)
 sess = rt.InferenceSession("./pytorch_custom_op/model_custom_op.onnx", so1)
-
